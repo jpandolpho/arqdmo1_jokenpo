77e5ba67a64fe5c3cb01ff3aff2cf20a40148656
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-12-09 10:42:04 -0300

    Reestruturando projeto para modelo MVVM.

--- app/src/main/java/br/edu/ifsp/dmo1/jokenpo/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/jokenpo/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package br.edu.ifsp.dmo1.jokenpo
+package br.edu.ifsp.dmo1.jokenpo.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.dmo1.jokenpo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
