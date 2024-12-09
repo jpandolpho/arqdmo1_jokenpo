bb42c04ce6f6f6d9666bf2900c1022244cfc9384
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-12-09 11:03:20 -0300

    Criando lógica da MainActivity.

--- app/src/main/java/br/edu/ifsp/dmo1/jokenpo/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/jokenpo/view/MainActivity.kt
@@ -1,21 +1,56 @@
 package br.edu.ifsp.dmo1.jokenpo.view
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import br.edu.ifsp.dmo1.jokenpo.R
+import br.edu.ifsp.dmo1.jokenpo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate((layoutInflater))
+        setContentView(binding.root)
+
+        configToolBar()
+        configSpinner()
+        configListener()
     }
+
+    private fun configToolBar() {
+        supportActionBar?.hide()
+    }
+
+    private fun configSpinner() {
+        val adapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_spinner_dropdown_item,
+            resources.getStringArray(R.array.tipos_jogos)
+        )
+        binding.spinnerBattles.adapter = adapter
+    }
+
+    private fun configListener() {
+        binding.buttonStart.setOnClickListener { startGame() }
+    }
+
+    private fun startGame() {
+        val battles: Int = when(binding.spinnerBattles.selectedItemPosition){
+            0 -> 1
+            1 -> 3
+            else -> 5
+        }
+
+        val mIntent = Intent(this, WarActivity::class.java)
+        mIntent.putExtra(Constants.KEY_PLAYER_1,binding.edittextPlayer1.text.toString())
+        mIntent.putExtra(Constants.KEY_PLAYER_2,binding.edittextPlayer2.text.toString())
+        mIntent.putExtra(Constants.KEY_ROUNDS,battles)
+        startActivity(mIntent)
+    }
+
+
 }
