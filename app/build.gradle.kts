5a70fb1ac536294a112bab79e3573a8c15092f08
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-12-12 15:30:02 -0300

    Definindo lógica da WarActivity.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "br.edu.ifsp.dmo1.jokenpo"
-        minSdk = 30
+        minSdk = 33
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
