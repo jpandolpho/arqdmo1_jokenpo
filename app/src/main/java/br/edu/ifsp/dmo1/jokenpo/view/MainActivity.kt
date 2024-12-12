56b2c3b576e0cf79ba7b9b0e9262b7b98008e7ee
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-12-12 16:01:55 -0300

    Ajustando Manifest.

--- app/src/main/java/br/edu/ifsp/dmo1/jokenpo/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/jokenpo/view/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate((layoutInflater))
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         configToolBar()
